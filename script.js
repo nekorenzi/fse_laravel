b1921f940445aaea147c401554ee5b5e9d8c1d25
nekorenzi <163225147+nekorenzi@users.noreply.github.com>
2024-03-13 15:44:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-document.getElementById('copy-button').addEventListener('click', function() {
-  var codeBlock = document.getElementById('code-block');
-  var range = document.createRange();
-  range.selectNode(codeBlock);
-  window.getSelection().addRange(range);
-  document.execCommand('copy');
-  window.getSelection().removeAllRanges();
+document.querySelectorAll('.copy-button').forEach(button => {
+  button.addEventListener('click', function() {
+      // コードブロックを囲む親要素を取得
+      var codeWrapper = this.closest('.code-wrap');
+      if (!codeWrapper) {
+          console.error('コードブロックを囲む親要素が見つかりませんでした。');
+          return;
+      }
+      
+      // 親要素からコードブロックを取得
+      var codeBlock = codeWrapper.querySelector('.code-block');
+      if (!codeBlock) {
+          console.error('コードブロックが見つかりませんでした。');
+          return;
+      }
+      
+      // コードブロックのテキストを選択してコピー
+      var range = document.createRange();
+      range.selectNode(codeBlock);
+      window.getSelection().addRange(range);
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
+  });
 });
+
 document.addEventListener("DOMContentLoaded", function() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "sidemenu.html", true);
