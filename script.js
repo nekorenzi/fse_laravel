49e7bd3755f65756992d7dc4446d605b2b5554e4
nekorenzi <163225147+nekorenzi@users.noreply.github.com>
2024-03-22 13:55:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 document.querySelectorAll('.copy-button').forEach(button => {
     button.addEventListener('click', function() {
-        // コードブロックを囲む親要素を取得
         var codeWrapper = this.closest('.code-wrap');
         if (!codeWrapper) {
             console.error('コードブロックを囲む親要素が見つかりませんでした。');
             return;
         }
         
-        // 親要素からコードブロックを取得
         var codeBlock = codeWrapper.querySelector('.code-block');
         if (!codeBlock) {
             console.error('コードブロックが見つかりませんでした。');
             return;
         }
         
-        // コードブロックのテキストを取得してクリップボードにコピー
         var codeText = codeBlock.innerText;
         navigator.clipboard.writeText(codeText)
             .then(() => console.log('コピーが成功しました。'))
             .catch(err => console.error('コピー中にエラーが発生しました:', err));
     });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "sidemenu.html", true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById("sidemenu").innerHTML = xhr.responseText;
-        }
-    };
-    xhr.send();
-  });
-  document.addEventListener("DOMContentLoaded", function() {
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     // すべてのコードブロックをシンタックスハイライト
     document.querySelectorAll('pre code').forEach((block) => {
         hljs.highlightElement(block);
