e32524f3ac551d332983b39d6d3a058d456f42b1
nekorenzi <163225147+nekorenzi@users.noreply.github.com>
2024-03-22 13:56:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,32 +18,3 @@ document.querySelectorAll('.copy-button').forEach(button => {
             .catch(err => console.error('コピー中にエラーが発生しました:', err));
     });
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    // すべてのコードブロックをシンタックスハイライト
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightElement(block);
-    });
-
-    // コピーの処理はそのまま利用できます
-    document.querySelectorAll('.copy-button').forEach(button => {
-        button.addEventListener('click', function() {
-            var codeWrapper = this.closest('.code-wrap');
-            if (!codeWrapper) {
-                console.error('コードブロックを囲む親要素が見つかりませんでした。');
-                return;
-            }
-            
-            var codeBlock = codeWrapper.querySelector('.code-block');
-            if (!codeBlock) {
-                console.error('コードブロックが見つかりませんでした。');
-                return;
-            }
-            
-            var codeText = codeBlock.innerText;
-            navigator.clipboard.writeText(codeText)
-                .then(() => console.log('コピーが成功しました。'))
-                .catch(err => console.error('コピー中にエラーが発生しました:', err));
-        });
-    });
-});
